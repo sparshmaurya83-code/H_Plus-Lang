4f786773f278b9a148ec3b7f06492601c3846fff
sparshmaurya83-code <sparshmaurya83@gmail.com>
2026-03-23 16:38:36 +0530

    --

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,32 @@
+
+use std::env;
+use std::fs;
+
+mod runtime;
 mod lexer;
 mod parser;
 mod ast;
 mod interpreter;
 
-use std::io::{self, Write};
-use std::fs;
-
 use lexer::tokenize;
 use parser::Parser;
 use interpreter::Interpreter;
 
-fn run_code(code: &str, interpreter: &mut Interpreter) {
-    let tokens = tokenize(code);
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: hplus <file.hplus>");
+        return;
+    }
+
+    let code = fs::read_to_string(&args[1])
+        .expect("Failed to read file");
+
+    let tokens = tokenize(&code);
     let mut parser = Parser::new(tokens);
     let ast = parser.parse();
 
-    interpreter.run(ast);
-}
-
-fn main() {
     let mut interpreter = Interpreter::new();
-
-    println!("🔥 H+ REPL (type 'exit' to quit)");
-
-    loop {
-        print!("> ");
-        io::stdout().flush().unwrap();
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-
-        if input.trim() == "exit" {
-            break;
-        }
-
-        run_code(&input, &mut interpreter);
-    }
+    interpreter.run(ast);
 }
